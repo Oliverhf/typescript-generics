875534eb8bca370dbf0e00b19ecb87c501235db0
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-09-25 18:13:28 -0300

    Type Constraints to Generic Functions

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-function shortenArray(data, amountToShorten) {
-    return data.splice(amountToShorten, data.length);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var conferenceRoom_1 = require("./models/conferenceRoom");
+function getBigRooms(rooms, minSize) {
+    var bigRooms = [];
+    rooms.forEach(function (r) {
+        if (r.capacity > minSize) {
+            bigRooms.push(r);
+        }
+    });
+    return bigRooms;
 }
-var stringArray = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
-var fewerLanguages = shortenArray(stringArray, 2);
-console.log(fewerLanguages);
+var bigRooms = getBigRooms(conferenceRoom_1.conferenceRoomData, 20);
+console.log(bigRooms);
+// function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+//     return data.splice(amountToShorten, data.length);
+// }
+// let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
+// let fewerLanguages = shortenArray(stringArray,2);
+// console.log(fewerLanguages);
--- main.ts
+++ main.ts
@@ -1,9 +1,32 @@
-function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
-    return data.splice(amountToShorten, data.length);
+import { MeetingResource } from "./models/meetingResource";
+import { ConferenceRoom, conferenceRoomData } from "./models/conferenceRoom";
+import { PartyTent, partyTentData } from "./models/partyTent";
+import { Building, buildingData } from "./models/building";
+
+function getBigRooms<T extends MeetingResource>(rooms: Array<T>, minSize: number): Array<T> {
+
+    let bigRooms: Array<T> = [];
+
+    rooms.forEach(r => {
+        if(r.capacity > minSize) {
+            bigRooms.push(r);
+        }
+    })
+
+    return bigRooms;
 }
 
-let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
+let bigRooms: Array<ConferenceRoom> = getBigRooms<ConferenceRoom>(conferenceRoomData, 20);
 
-let fewerLanguages = shortenArray(stringArray,2);
+console.log(bigRooms);
 
-console.log(fewerLanguages);
+
+// function shortenArray<T>(data: Array<T>, amountToShorten: number): Array<T> {
+//     return data.splice(amountToShorten, data.length);
+// }
+
+// let stringArray: string[] = ['Visual Basic', 'C++', 'TypeScript', 'JavaScript'];
+
+// let fewerLanguages = shortenArray(stringArray,2);
+
+// console.log(fewerLanguages);
