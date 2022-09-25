71932cb7fe9c22ec2d9310b94c92c19004766aa6
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-09-25 18:49:47 -0300

    Applying type constraints to generic classes

---
+++ models/reservation.ts
@@ -0,0 +1,19 @@
+import { ConferenceRoom, conferenceRoomData } from "./conferenceRoom";
+import { MeetingResource } from "./meetingResource";
+
+
+class Reservation<T extends MeetingResource> {
+    reservationDate: Date;
+    organizerName: string;
+    resource: T;
+
+    requestResource(requestedResource: T, requester: string) { // Generic classes are only generic over their instances.
+        this.resource = requestedResource;
+        this.organizerName = requester;
+        console.log(`${requester} requested a reservation for ${requestedResource.name}`);
+    }
+}
+
+
+let teamMeeting = new Reservation<ConferenceRoom>();
+teamMeeting.requestResource(conferenceRoomData[0], 'Gary');
